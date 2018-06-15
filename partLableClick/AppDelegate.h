ff373ac3f916401e2d11965b24fa25f51659a8ad
wangmingliang <wangmingliang@idengyun.com>
2018-06-15 16:48:44 +0800

    Initial Commit

---
+++ partLableClick/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  partLableClick
+//
+//  Created by 王明亮 on 2018/6/15.
+//  Copyright © 2018年 王明亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
